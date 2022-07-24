66adb801433f511587f4e2aeb467446bd772cf36
Louisse Laine <eledalouisse@gmail.com>
2022-07-24 16:56:32 +0800

    Angular Data Binding (INTERPOLATION) - Tab 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  fn = "Louisse Laine B. Eleda";
+  nick = "Leyn";
+  bday = "May 30, 2001";
+  addr = "Durungao, Bauan, Batangas";
+
   constructor() {}
 
 }
